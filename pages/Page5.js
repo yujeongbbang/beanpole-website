0787a3162ba58571da3b0d1c3433a8bc56a8871c
yujeongbbang <137030883+yujeongbbang@users.noreply.github.com>
2024-06-26 15:42:44 +0900

    feat: Page5 구조

--- pages/Page5.js
+++ pages/Page5.js
@@ -1,9 +1,19 @@
 import styles from '../styles/Page5.module.css';
 
-const Page5 = () => (
-  <div className={styles.page5Container}>
-    <h1>Page 5</h1>
-  </div>
-);
-
+const Page5 = () => {
+  return (
+    <div className={styles.page5Container}>
+      <div className={styles.page5TextContainer}>
+        <h1 className={styles.page5Title}>GOOD ACTIVITY</h1>
+        <p className={styles.page5Subtitle}>
+         좋은 옷을 더 오래 입는 방법
+        </p>
+        <div className={styles.page5Category}>
+          <span className={styles.page5CateList}>GOOD CARE</span>
+          <span className={styles.page5CateList}>GOOD CYCLE</span>
+          </div>
+      </div>
+    </div>
+  );
+};
 export default Page5;
